caa4dd950ff8dff43a69928a20f16354d4dc4ca9
isobeledmonds <edmonds.isobel@gmail.com>
2024-05-03 17:26:54 +0800

    selection

--- question.js
+++ question.js
@@ -2,6 +2,8 @@
 
 
 let lastClicked = null;
+let results = [];
+let nextButton = document.getElementById("nextButton");
 
     function handleButtonClick(buttonId, currentButton) {
         // Remove 'clicked' class from previously clicked button
@@ -13,6 +15,7 @@ let lastClicked = null;
         if (lastClicked !== null) {
             lastClicked.disabled = false;
         }
+
         
         // Disable the current button
         currentButton.disabled = true;
@@ -22,9 +25,26 @@ let lastClicked = null;
 
         // Update the lastClicked variable
         lastClicked = currentButton;
+
+        // Enable or disable the next button based on whether any button has been clicked
+        nextButton.removeAttribute("disabled");
+
+ }
+
+
+function logResults(event) {
+    if (nextButton.hasAttribute("disabled")) {
+    event.preventDefault();
     }
+    if (lastClicked !== null) {
+        results.push(lastClicked.getAttribute("id"));; // Push the buttonId to results array
+        console.log("Results:", results);
+    } else {
+        console.log("No button has been clicked yet.");
+    }
+}
 
 
 
-let results = []
+
 
